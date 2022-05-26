9deb969a1bcd24060a52dc3ffbd346efab1b6387
yin-ol <yin_ol@outlook.com>
2022-05-26 12:46:02 +0800

    新版本时钟app代码有混淆，现在改为hook底层一点的api: AudioManager.class，伪装没有在录音

--- app/src/main/java/cn/yhfcn/samsung_clockfix/mod/ClockAppHook.kt
+++ app/src/main/java/cn/yhfcn/samsung_clockfix/mod/ClockAppHook.kt
@@ -1,6 +1,6 @@
 package cn.yhfcn.samsung_clockfix.mod
 
-import android.content.Context
+import android.media.AudioManager
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedHelpers
@@ -10,41 +10,20 @@ class ClockAppHook: IXposedHookLoadPackage {
     @Throws(Throwable::class)
     override fun handleLoadPackage(lpParam: XC_LoadPackage.LoadPackageParam) {
         if (lpParam.packageName == "com.sec.android.app.clockpackage") {
-            val clazz = lpParam.classLoader.loadClass("com.sec.android.app.clockpackage.common.util.StateUtils")
-
-            XposedHelpers.findAndHookMethod(
-                clazz,
-                "isInCall",
-                Context::class.java,
-                object : XC_MethodHook() {
-                    @Throws(Throwable::class)
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        super.beforeHookedMethod(param)
-                    }
-
-                    @Throws(Throwable::class)
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        param.result = false
-                        super.afterHookedMethod(param)
-                    }
-                })
-
-            XposedHelpers.findAndHookMethod(
-                clazz,
-                "isInVoipCall",
-                Context::class.java,
-                object : XC_MethodHook() {
-                    @Throws(Throwable::class)
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        super.beforeHookedMethod(param)
-                    }
-
-                    @Throws(Throwable::class)
-                    override fun afterHookedMethod(param: MethodHookParam) {
-                        param.result = false
-                        super.afterHookedMethod(param)
-                    }
-                })
+            XposedHelpers.findAndHookMethod(AudioManager::class.java, "semIsRecordActive", Int::class.java, object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    param.result = false
+                }
+            })
+            /**
+             * @see android.media.AudioManager.getMode
+             * @see android.media.AudioManager.MODE_CALL_SCREENING = 4
+             */
+            XposedHelpers.findAndHookMethod(AudioManager::class.java, "getMode", object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    param.result = 4
+                }
+            })
         }
     }
 }
